c7109a5406ebe5ec6b3904b21c30d60e555b5bef
Alex Nuccio <anuccio1@gmail.com>
2015-04-10 21:01:20 -0500

    added model

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    let factBook = FactBook()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.factsArray[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +25,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
-        funFactLabel.text = "Another interesting fact"
+        funFactLabel.text = factBook.factsArray[1]
         
     }
 
